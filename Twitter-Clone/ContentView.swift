c5887fb82bfa1f47b7204733f46f9253631e8c78
Ejder Dag <ejdrdag@gmail.com>
2024-05-12 16:16:37 +0300

    Initial Commit

---
+++ Twitter-Clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Twitter-Clone
+//
+//  Created by Ejder Dağ on 12.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Twitter-Clone/Twitter_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_CloneApp.swift
+//  Twitter-Clone
+//
+//  Created by Ejder Dağ on 12.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
